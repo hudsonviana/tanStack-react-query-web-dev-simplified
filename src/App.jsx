3c4012f0a01b1dfa35d8122f215d1723821e7428
hudsonviana <hudson1206@gmail.com>
2024-06-20 23:15:05 -0300

    Basic Example

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,61 @@
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+
+const POSTS = [
+  { id: 1, title: 'Post 1' },
+  { id: 2, title: 'Post 2' },
+  { id: 3, title: 'Post 3' },
+];
+
 const App = () => {
+  const queryClient = useQueryClient();
+
+  const postsQuery = useQuery({
+    queryKey: ['posts'],
+    queryFn: () => wait(1000).then(() => [...POSTS]),
+    // queryFn: () => Promise.reject('Mensagem de erro'),
+  });
+
+  const newPostMutation = useMutation({
+    mutationFn: (title) => {
+      return wait(1000).then(() =>
+        POSTS.push({ id: crypto.randomUUID(), title })
+      );
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(['posts']);
+    },
+  });
+
+  if (postsQuery.isError) {
+    return <pre>{JSON.stringify(postsQuery.error)}</pre>;
+  }
+
   return (
     <>
       <h1>TanStack Query 2024</h1>
+      {console.log(newPostMutation.isLoading)}
+      <button
+        disabled={newPostMutation.isPending}
+        onClick={() => newPostMutation.mutate('Novo post')}
+      >
+        Adicionar
+      </button>
+      <br /> <br />
+      {postsQuery.isLoading ? (
+        <h2>Loading...</h2>
+      ) : (
+        <div>
+          {postsQuery.data.map((post) => (
+            <div key={post.id}>{post.title}</div>
+          ))}
+        </div>
+      )}
     </>
-  )
+  );
+};
+
+function wait(duration) {
+  return new Promise((resolve) => setTimeout(resolve, duration));
 }
 
-export default App
+export default App;
